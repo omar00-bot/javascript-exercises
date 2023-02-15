459052c40cb304e669b8b53c22a1ce11f3f1fbd6
Omar A. Orga <orgaomar8@gmail.com>
2023-02-15 16:28:21 +0800

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLength = string.length;
+    console.log(stringLength);
+    let newString = ``;
+    let x = stringLength;
+    for(let i=0; i<stringLength; i++){
+        newString += string[x-1];
+        x--;
+    }
+    return newString;
 };
+
+console.log(reverseString(`Hello`));
 
 // Do not edit below this line
 module.exports = reverseString;
