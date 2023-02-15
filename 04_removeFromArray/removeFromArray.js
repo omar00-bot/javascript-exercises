146c418ecec2f62ae1f7f65c7abf550f7d967f14
Omar A. Orga <orgaomar8@gmail.com>
2023-02-15 19:36:14 +0800

    Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
 const removeFromArray = function() {
-
+    let argumentLength = arguments.length;
+    const result = [];
+    const arg0 = arguments[0];
+    
+    console.log(argumentLength);
+    console.log(arg0);
+    
+    for (let i=1; i<argumentLength; i++){
+        let deleteLength = 1;
+        const searchForString = arguments[argumentLength-i];
+        const indexToDelete = arg0.indexOf(searchForString);
+        console.log(indexToDelete);
+        if (indexToDelete === -1){
+            deleteLength = 0;
+        }
+        const result = arg0.splice(indexToDelete,deleteLength);
+    }
+    return arg0;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"));
 // Do not edit below this line
 module.exports = removeFromArray;
