9998dd8d33c6f11f9395b215bce9b5a29edfe2f7
Omar A. Orga <orgaomar8@gmail.com>
2023-02-15 22:15:33 +0800

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let min, max;
+    let sum = 0;
+    console.log(typeof a);
+    if (typeof a === 'number' && typeof b === 'number' && a>=0 && b>=0){ 
+        if(a>b){
+        min = b;
+        max = a;
+        }
+        else{
+        min = a;
+        max = b;
+        }
+    }
+    else {
+        return `ERROR`;
+    }
+    for (let i=min; i<=max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
+console.log(sumAll(-10, 4));
 // Do not edit below this line
 module.exports = sumAll;
