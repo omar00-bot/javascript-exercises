fb0ba692d34a35dbb8a6e00e1098d98f2de1b6b2
Omar A. Orga <orgaomar8@gmail.com>
2023-03-04 23:48:59 +0800

    Update

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+return books.map((book => book.title))
 };
 
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    },
+    {
+        title: `talo2`,
+        author: `boang`
+    }
+  ]
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,39 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(obj => {
+        if(!(`yearOfDeath` in obj)){
+            obj.yearOfDeath = new Date().getFullYear();
+            console.log(obj.yearOfDeath-obj.yearOfBirth)
+        }
+    });
+    oldest = people.sort((a , b) => {
+    const ageA = a.yearOfDeath-a.yearOfBirth;
+    console.log(ageA)
+    const ageB = b.yearOfDeath-b.yearOfBirth;
+    console.log(ageA)
+    return ageB-ageA;
+    }
+    )
+return oldest[0]
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  console.log(findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
